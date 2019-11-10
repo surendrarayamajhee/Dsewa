a4f4bc506711bc80cd44ca63b6db0f7be43c1ebb
su-477 <chaudharysuraj477@gmail.com>
2019-11-10 12:54:33 +0545

    ok

--- resources/js/components/home.vue
+++ resources/js/components/home.vue
@@ -4,13 +4,35 @@
         <div class="row justify-content-center">
             <div class="container-fluid mt-3">
                 <div class="row">
-
-                     <div class="col-xl-3 col-lg-6" style="width: 200px; height:180px;">
+                    <div class="col-xl-3 col-lg-6" style="width: 200px; height:180px;">
                         <div class="card card-stats mb-4 mb-xl-0" style="width: 200px; height:180px;">
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col">
                                         <h5 class="card-title text-uppercase text-muted mb-0">Orders</h5>
+                                            <span class="h4 font-weight-bold mb-0">350,897</span>
+                                    </div>
+                                    <div class="col-auto">
+                                        <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
+                                            <i class="fas fa-cart-plus"> 
+                                            </i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <p class="mt-3 mb-0 text-muted text-sm">
+                                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                                    <span class="text-nowrap">Since last month</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xl-3 col-lg-6" style="width: 200px; height:180px;">
+                        <div class="card card-stats mb-4 mb-xl-0" style="width: 200px; height:180px;">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col">
+                                        <h5 class="card-title text-uppercase text-muted mb-0"></h5>
                                             <span class="h4 font-weight-bold mb-0">350,897</span>
                                     </div>
                                     <div class="col-auto">
@@ -73,121 +95,8 @@
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    <div class="col-xl-3 col-lg-6" style="width: 200px; height:180px;">
-                        <div class="card card-stats mb-4 mb-xl-0" style="width: 200px; height:180px;">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col">
-                                        <h5 class="card-title text-uppercase text-muted mb-0">Orders</h5>
-                                            <span class="h4 font-weight-bold mb-0">350,897</span>
-                                    </div>
-                                    <div class="col-auto">
-                                        <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
-                                            <i class="fas fa-cart-plus"> 
-                                            </i>
-                                        </div>
-                                    </div>
-                                </div>
-                                <p class="mt-3 mb-0 text-muted text-sm">
-                                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                                    <span class="text-nowrap">Since last month</span>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-xl-3 col-lg-6" style="width: 200px; height:180px;">
-                        <div class="card card-stats mb-4 mb-xl-0" style="width: 200px; height:180px;">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col">
-                                        <h5 class="card-title text-uppercase text-muted mb-0">Orders</h5>
-                                            <span class="h4 font-weight-bold mb-0">350,897</span>
-                                    </div>
-                                    <div class="col-auto">
-                                        <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
-                                            <i class="fas fa-cart-plus"> 
-                                            </i>
-                                        </div>
-                                    </div>
-                                </div>
-                                <p class="mt-3 mb-0 text-muted text-sm">
-                                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                                    <span class="text-nowrap">Since last month</span>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-xl-3 col-lg-6" style="width: 200px; height:180px;">
-                        <div class="card card-stats mb-4 mb-xl-0" style="width: 200px; height:180px;">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col">
-                                        <h5 class="card-title text-uppercase text-muted mb-0">Orders</h5>
-                                            <span class="h4 font-weight-bold mb-0">350,897</span>
-                                    </div>
-                                    <div class="col-auto">
-                                        <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
-                                            <i class="fas fa-cart-plus"> 
-                                            </i>
-                                        </div>
-                                    </div>
-                                </div>
-                                <p class="mt-3 mb-0 text-muted text-sm">
-                                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                                    <span class="text-nowrap">Since last month</span>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-xl-3 col-lg-6" style="width: 200px; height:180px;">
-                        <div class="card card-stats mb-4 mb-xl-0" style="width: 200px; height:180px;">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col">
-                                        <h5 class="card-title text-uppercase text-muted mb-0">Orders</h5>
-                                            <span class="h4 font-weight-bold mb-0">350,897</span>
-                                    </div>
-                                    <div class="col-auto">
-                                        <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
-                                            <i class="fas fa-cart-plus"> 
-                                            </i>
-                                        </div>
-                                    </div>
-                                </div>
-                                <p class="mt-3 mb-0 text-muted text-sm">
-                                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                                    <span class="text-nowrap">Since last month</span>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-xl-3 col-lg-6" style="width: 200px; height:180px;">
-                        <div class="card card-stats mb-4 mb-xl-0" style="width: 200px; height:180px;">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col">
-                                        <h5 class="card-title text-uppercase text-muted mb-0">Orders</h5>
-                                            <span class="h4 font-weight-bold mb-0">350,897</span>
-                                    </div>
-                                    <div class="col-auto">
-                                        <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
-                                            <i class="fas fa-cart-plus"> 
-                                            </i>
-                                        </div>
-                                    </div>
-                                </div>
-                                <p class="mt-3 mb-0 text-muted text-sm">
-                                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                                    <span class="text-nowrap">Since last month</span>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
                     <!-- <div class="col-lg-3 col-sm-6">
                         <div class="card gradient-1">
                             <div class="card-body">
@@ -332,7 +241,6 @@
                             </div>
                         </div>
                     </div> -->
-                </div>
                 
 
                 <div class="row">
