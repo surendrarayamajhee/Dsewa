37dd7d5cfecb02e4600198ef76cdb44a1c9d847a
su-477 <chaudharysuraj477@gmail.com>
2019-11-11 15:56:24 +0545

    ok

--- resources/js/components/home.vue
+++ resources/js/components/home.vue
@@ -1,7 +1,5 @@
 <template>
 <div>
-
-    
     <div class="content-body">
         <div class="row justify-content-center">
             <div class="container-fluid mt-3">
@@ -18,7 +16,7 @@
                         </div>
                     </div>
                         <!-- second-box -->
-                     <div class="flex-container">
+                    <div class="flex-container">
                         <div class="Cart">
                             <span style="font-size: 3em; color: royalblue;">
                             <i class="fas fa-cart-plus"></i>
@@ -29,7 +27,7 @@
                         </div>
                     </div>
                         <!-- Third-Box -->
-                     <div class="flex-container">
+                    <div class="flex-container">
                         <div class="Cart">
                             <span style="font-size: 3em; color: royalblue;">
                             <i class="fas fa-cart-plus"></i>
@@ -40,7 +38,7 @@
                         </div>
                     </div>
                         <!-- Fouth-Box -->
-                     <div class="flex-container">
+                    <div class="flex-container">
                         <div class="Cart">
                             <span style="font-size: 3em; color: royalblue;">
                             <i class="fas fa-cart-plus"></i>
@@ -48,76 +46,11 @@
                         </div>
                         <div class="headline">
                             <div>Orders</div>
-                <div class="row">
-                    <div class="flex lg3 sm6 xs12">
-                        <div class="v-card v-sheet theme--light">
-                            <div class="v-card__text pa-0">
-                                <div class="container pa-0">
-                                    <div class="f2">
-                                        <div class="focus">
-                                            <div class="layout column ma-0 justify-center align-center">
-                                                <span style="font-size: 48px; color: Dodgerblue;">
-                                                <i aria-hidden="true" class="fas fa-cart-plus"></i>
-                                                </span>
-                                            </div>
-                                        </div>
-                                       <!-- <div class="focus">
-                                            <div class="headline">100</div>
-                                            <span class="caption">Orders</span>
-                                        </div> -->
-                                    </div>
-                                </div>
-                                
-                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-                <!-- <div class="row">
-                    <div class="col-lg-12">
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="card">
-                                    <div class="card-body pb-0 d-flex justify-content-between">
-                                        <div>
-                                            <h4 class="mb-1">Product Sales</h4>
-                                            <p>Total Earnings of the Month</p>
-                                            <h3 class="m-0">$ 12,555</h3>
-                                        </div>
-                                        <div>
-                                            <ul>
-                                                <li class="d-inline-block mr-3"><a class="text-dark" href="#">Day</a></li>
-                                                <li class="d-inline-block mr-3"><a class="text-dark" href="#">Week</a></li>
-                                                <li class="d-inline-block"><a class="text-dark" href="#">Month</a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    <div class="chart-wrapper">
-                                        <canvas id="chart_widget_2"></canvas>
-                                    </div>
-                                    <div class="card-body">
-                                        <div class="d-flex justify-content-between">
-                                            <div class="w-100 mr-2">
-                                                <h6>Pixel 2</h6>
-                                                <div class="progress" style="height: 6px">
-                                                    <div class="progress-bar bg-danger" style="width: 40%"></div>
-                                                </div>
-                                            </div>
-                                            <div class="ml-2 w-100">
-                                                <h6>iPhone X</h6>
-                                                <div class="progress" style="height: 6px">
-                                                    <div class="progress-bar bg-primary" style="width: 80%"></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div> -->
-     
     </div>
 </div>  
                 
